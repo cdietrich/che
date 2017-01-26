5155d346adfb9e113853e4265970e2ac83031a2b
Christian Dietrich <christian.dietrich@itemis.de>
2017-01-26 13:01:09 +0100

    initial checkin
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>
    
    fixed broken agent script
    
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

---
+++ plugins/plugin-mydsl/che-plugin-mydsl-lang-server/src/main/java/org/eclipse/che/plugin/mydsl/languageserver/MyDslLanguageServerLauncher.java
@@ -0,0 +1,82 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2017 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Christian Dietrich - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.plugin.mydsl.languageserver;
+
+import static java.util.Arrays.asList;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.eclipse.che.api.languageserver.exception.LanguageServerException;
+import org.eclipse.che.api.languageserver.launcher.LanguageServerLauncherTemplate;
+import org.eclipse.che.api.languageserver.shared.model.LanguageDescription;
+import org.eclipse.che.api.languageserver.shared.model.impl.LanguageDescriptionImpl;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import io.typefox.lsapi.services.json.JsonBasedLanguageServer;
+
+/**
+ * @author Christian Dietrich
+ */
+@SuppressWarnings("deprecation")
+@Singleton
+public class MyDslLanguageServerLauncher extends LanguageServerLauncherTemplate {
+
+    private static final String   LANGUAGE_ID = "mydsl";
+    private static final String[] EXTENSIONS  = new String[] {"mydsl"};
+    private static final String[] MIME_TYPES  = new String[] {"text/x-mydsl"};
+    private static final LanguageDescriptionImpl description;
+
+    private final Path launchScript;
+
+    static {
+        description = new LanguageDescriptionImpl();
+        description.setFileExtensions(asList(EXTENSIONS));
+        description.setLanguageId(LANGUAGE_ID);
+        description.setMimeTypes(asList(MIME_TYPES));
+    }
+
+    @Inject
+    public MyDslLanguageServerLauncher() {
+        launchScript = Paths.get(System.getenv("HOME"), "che/ls-mydsl/mydsl/bin/mydsl-standalone");
+    }
+
+    @Override
+    public LanguageDescription getLanguageDescription() {
+        return description;
+    }
+
+    @Override
+    public boolean isAbleToLaunch() {
+        return Files.exists(launchScript);
+    }
+
+    protected JsonBasedLanguageServer connectToLanguageServer(Process languageServerProcess) {
+        JsonBasedLanguageServer languageServer = new MyDslLanguageServer();
+        languageServer.connect(languageServerProcess.getInputStream(), languageServerProcess.getOutputStream());
+        return languageServer;
+    }
+
+    protected Process startLanguageServerProcess(String projectPath) throws LanguageServerException {
+        ProcessBuilder processBuilder = new ProcessBuilder(launchScript.toString());
+        processBuilder.redirectInput(ProcessBuilder.Redirect.PIPE);
+        processBuilder.redirectOutput(ProcessBuilder.Redirect.PIPE);
+        try {
+            return processBuilder.start();
+        } catch (IOException e) {
+            throw new LanguageServerException("Can't start JSON language server", e);
+        }
+    }
+}
